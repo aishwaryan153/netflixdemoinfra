726d628b382ac7697a3c60c7290b3142c0660104
aishwaryan153 <aishwaryalaxmi0599@gmail.com>
2025-04-12 08:32:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-00a929b66ed6e0de6 "
+ami = "ami-00a929b66ed6e0de6"
 instance_type = "t2.medium"
 key_name = "new"
 vpc_security_group_ids = ["sg-035024ae7cf33b5b6"]
