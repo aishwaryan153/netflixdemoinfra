dc6ed51965299db911fc16283f75d206be4d01a8
aishwaryan153 <aishwaryalaxmi0599@gmail.com>
2025-04-12 08:29:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-00a929b66ed6e0de6 "
 instance_type = "t2.medium"
-key_name = "swikp"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "new"
+vpc_security_group_ids = ["sg-035024ae7cf33b5b6"]
 tags = {
 Name = var.instance_names[count.index]
 }
